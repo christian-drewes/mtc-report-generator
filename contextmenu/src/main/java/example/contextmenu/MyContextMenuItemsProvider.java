59b5cffa321ca44114820ad8b3733194333ff1b6
christian-drewes <cddok20@gmail.com>
2024-10-16 14:30:49 -0500

    build DOCX Test File

--- contextmenu/src/main/java/example/contextmenu/MyContextMenuItemsProvider.java
+++ contextmenu/src/main/java/example/contextmenu/MyContextMenuItemsProvider.java
@@ -14,10 +14,17 @@ import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MyContextMenuItemsProvider implements ContextMenuItemsProvider
 {
@@ -38,11 +45,16 @@ public class MyContextMenuItemsProvider implements ContextMenuItemsProvider
 
             JMenuItem retrieveRequestItem = new JMenuItem("Print request");
             JMenuItem retrieveResponseItem = new JMenuItem("Print response");
+            JMenuItem captureMTC = new JMenuItem("Print MTC1A-TEST4");
 
             HttpRequestResponse requestResponse = event.messageEditorRequestResponse().isPresent() ? event.messageEditorRequestResponse().get().requestResponse() : event.selectedRequestResponses().get(0);
 
             retrieveRequestItem.addActionListener(l -> api.logging().logToOutput("Request is:\r\n" + requestResponse.request().toString()));
             menuItemList.add(retrieveRequestItem);
+            menuItemList.add(captureMTC);
+
+            String endpoint = requestResponse.request().path();
+            Short statusCode = requestResponse.response().statusCode();
 
             if (requestResponse.response() != null)
             {
@@ -50,9 +62,71 @@ public class MyContextMenuItemsProvider implements ContextMenuItemsProvider
                 menuItemList.add(retrieveResponseItem);
             }
 
+            // MTC Request/Response capture
+            captureMTC.addActionListener(l -> {
+
+                api.logging().logToOutput("Request is:\r\n" + requestResponse.request().toString());
+
+                try {
+                    api.logging().logToOutput("BEFORE");
+                    XWPFDocument document = new XWPFDocument();
+
+                    //Write the Document in file system
+                    FileOutputStream out = new FileOutputStream( new File("C:/tmp/createdocument.docx"));
+
+                    //create Paragraph
+                    XWPFRun run = newLine(document, true);
+                    run.setText(endpoint + " API call:\r\n");
+                    run = newLine(document, false);
+                    run.setText(requestResponse.request().toString() + "\r\n");
+
+                    if (requestResponse.response() != null && statusCode <= 301)
+                    {
+                        run = newLine(document, true);
+                        run.setText("Correct allow responses:");
+                        run = newLine(document, false);
+                        run.setText(requestResponse.response().toString());
+                    } else if (requestResponse.response() != null && statusCode > 301) {
+                        run = newLine(document, true);
+                        run.setText("Incorrect DENY response:");
+                        run = newLine(document, false);
+                        run.setText(requestResponse.response().toString());
+                    } else {
+                        api.logging().logToOutput("Response is:\r\n No Response");
+                        run.setText("No Response");
+                    }
+
+                    document.write(out);
+                    out.close();
+
+                } catch (Exception e) {
+                    api.logging().logToOutput("Don't freak out: " + e);
+                }
+            });
+
             return menuItemList;
         }
 
         return null;
+    }
+
+    XWPFRun newLine(XWPFDocument document, Boolean isBold){
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
+        run.setFontFamily("Arial");
+        run.setFontSize(9);
+        run.setBold(isBold);
+
+        return run;
+    }
+
+    XWPFRun newLine(XWPFDocument document){
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
+        run.setFontFamily("Arial");
+        run.setFontSize(9);
+        run.setBold(false);
+
+        return run;
     }
 }
